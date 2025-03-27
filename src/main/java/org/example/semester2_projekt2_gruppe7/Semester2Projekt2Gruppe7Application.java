63e118a3d35ff2438fcddc3850cc752793d131a0
Rummelhoff7 <mads.jack.rummelhoff@hotmail.com>
2025-03-27 10:03:42 +0100

    Initial commit

---
+++ src/main/java/org/example/semester2_projekt2_gruppe7/Semester2Projekt2Gruppe7Application.java
@@ -0,0 +1,13 @@
+package org.example.semester2_projekt2_gruppe7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Semester2Projekt2Gruppe7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Semester2Projekt2Gruppe7Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/semester2_projekt2_gruppe7/Semester2Projekt2Gruppe7ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.semester2_projekt2_gruppe7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Semester2Projekt2Gruppe7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
