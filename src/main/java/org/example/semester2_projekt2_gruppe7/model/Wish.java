7709644eb55d799ebe93ffcb728c311efcd2fa9a
guneyozcan <guoz0001@stud.kea.dk>
2025-04-03 12:11:08 +0200

    jkhasd

---
+++ src/main/java/org/example/semester2_projekt2_gruppe7/model/Wish.java
@@ -0,0 +1,67 @@
+package org.example.semester2_projekt2_gruppe7.model;
+
+public class Wish {
+    int id;
+    int user_id;
+    String name;
+    String description;
+    String img;
+
+    Wish(int id, int user_id, String name, String description, String img) {
+        this.id = id;
+        this.user_id = user_id;
+        this.name = name;
+        this.description = description;
+        this.img = img;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public String toString(){
+        return "id "+id+ "user_id"+user_id+"name"+name +"description"+description+"img "+img;
+    }
+
+    }
+
+
+
+
