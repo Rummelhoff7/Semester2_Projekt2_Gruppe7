e3e95dc85c761e64a9f994bc96310cf793101142
Joadoa1992 <jolu0001@stud.kea.dk>
2025-04-03 11:45:03 +0200

    SQL TABLES

--- src/test/Test.sql
+++ src/test/Test.sql
@@ -1,17 +1,38 @@
-DROP DATABASE IF EXISTS wishlist;
+DROP DATABASE IF EXISTS gaveListen;
 
-CREATE DATABASE wishlist;
+CREATE DATABASE gaveListen;
 
-USE wishlist;
+USE gaveListen;
 
 CREATE TABLE user (
                       id INT AUTO_INCREMENT PRIMARY KEY,
                       name VARCHAR(100) NOT NULL,
-                      email VARCHAR(100) NOT NULL,
                       password VARCHAR(50) NOT NULL
 );
 
 
 -- Dummy data, Det er de samme objekter som bruges i InitData.
-INSERT INTO user (name, email, password) VALUES
-    ('Mads', 'Mads@spillerbas', '1234');
+INSERT INTO user (name, password) VALUES
+    ('Mads', '1234');
+
+CREATE TABLE wishList(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL ,
+    name VARCHAR(50) NOT NULL ,
+    img VARCHAR(1000)
+);
+
+INSERT INTO wishList(user_id, name, img) VALUES
+    (2, 'Juleønsker', 'COOLPIC.png');
+
+CREATE TABLE wish(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    wishlist_id int NOT NULL ,
+    name VARCHAR(50) NOT NULL ,
+    description VARCHAR(100),
+    price DOUBLE,
+    img VARCHAR(1000)
+);
+
+INSERT INTO wish(wishlist_id, name, description,price, img) VALUES
+    (1,'Bil','Den skal være blå og stor',20000.50, 'coolpic2.png')
