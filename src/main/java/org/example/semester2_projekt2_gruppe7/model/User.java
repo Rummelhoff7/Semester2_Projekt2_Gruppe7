ffcddc92bd09b2852e0b84e25c29ec5d93c1e884
Rummelhoff7 <mads.jack.rummelhoff@hotmail.com>
2025-04-03 12:01:21 +0200

    User klasse oprettet

--- src/main/java/org/example/semester2_projekt2_gruppe7/model/User.java
+++ src/main/java/org/example/semester2_projekt2_gruppe7/model/User.java
@@ -1,4 +1,51 @@
 package org.example.semester2_projekt2_gruppe7.model;
 
-public class User {
+public class User { //Variabler skal nok være private.
+    int id;
+    String name;
+    String password;
+
+    public User(int id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public User() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    public String toString() {
+        return "User{" + "id=" + id + ", name=" + name + ", password=" + password + '}';
+    }
 }
