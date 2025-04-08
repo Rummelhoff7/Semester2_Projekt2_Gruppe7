3b23791ca56b1ed8530da6f49a7c3199df73d4af
Rummelhoff7 <mads.jack.rummelhoff@hotmail.com>
2025-04-08 11:19:04 +0200

    Rettelse i Controller, tilføjet

--- src/main/java/org/example/semester2_projekt2_gruppe7/controller/PageController.java
+++ src/main/java/org/example/semester2_projekt2_gruppe7/controller/PageController.java
@@ -1,19 +1,32 @@
 package org.example.semester2_projekt2_gruppe7.controller;
 
+import org.example.semester2_projekt2_gruppe7.model.WishList;
+import org.example.semester2_projekt2_gruppe7.repository.WishListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
 
 @Controller
 public class PageController {
-    @GetMapping("/")
-    public  String MainPage (){
-        return "index";
-    }
+
+
+    @Autowired
+    WishListRepository wishListRepo;
 
     @GetMapping("/wishlist")
-    public String wishlistPage(){
-    return "wishlist";
+    public String wishlistPage() {
+        return "wishlist";
     }
 
+    @GetMapping("/")
+    public String mainPage(Model model) {
+        ArrayList<WishList> wishList = wishListRepo.getAllWishList();
+        model.addAttribute("wishList", wishList);
 
+        return "index";
+
+    }
 }
